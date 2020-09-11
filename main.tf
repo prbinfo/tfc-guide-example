9ccef00c467da8dc8b09d720265ea6d97e602f20
prbinfo <68828661+prbinfo@users.noreply.github.com>
2020-09-11 06:57:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,12 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_instance" "basic" {
+  ami           = "ami-0ee1a20d6b0c6a347"
+  instance_type = "t3.nano"
+}
+
+
 provider "random" {
   version = "2.2"
 }
